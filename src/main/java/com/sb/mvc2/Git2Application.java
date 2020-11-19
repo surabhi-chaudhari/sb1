25e3f4acb3414fdca91dc2fbeac0a9d1cafce8d7
surabhi <surabhichaudhari96@gmail.com>
2020-11-19 22:45:19 +0530

    first commit

---
+++ src/main/java/com/sb/mvc2/Git2Application.java
@@ -0,0 +1,13 @@
+package com.sb.mvc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/mvc2/Git2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.mvc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
